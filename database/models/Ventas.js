fb4ed5632751ff4e87884e13e1c89aa0a70b900c
curyAilen <cury.ailena@gmail.com>
2022-12-29 16:24:34 -0300

    arreglo css y nueva databese

--- database/models/Ventas.js
+++ database/models/Ventas.js
@@ -1,33 +1,33 @@
 module.exports = (sequelize, dataTypes) => {
-    let alias = 'Ventas'; 
+    let alias = 'Ventas';
     let cols = {
-            idVentas: {
-                type: dataTypes.INTEGER.UNSIGNED,
-                primaryKey: true,
-                autoIncrement: true
-            }, 
-            idProductos: {
-                type: dataTypes.INTEGER,
-                allowNull: false
-            }, 
-            idUsuarios: {
-                type: dataTypes.INTEGER,
-                allowNull: false
-            },     
-            precio: {
-                type: dataTypes.INTEGER,
-                allowNull: false
-            },
-      
+        idVentas: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        idProductos: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        idUsuarios: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        importe: {
+            type: dataTypes.DECIMAL,
+            allowNull: false
+        },
+
     };
     let config = {
         timestamps: false,
         tableName: 'ventas',
-        freezeTableName:true
+        freezeTableName: true
     }
-    const Ventas = sequelize.define(alias,cols,config);
+    const Ventas = sequelize.define(alias, cols, config);
 
-    
-    
+
+
     return Ventas
 };
