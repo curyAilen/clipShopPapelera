dfe0c847683e5ea0e2692ba84482ed7607d96543
curyAilen <cury.ailena@gmail.com>
2023-01-07 15:17:28 -0300

    Actualización base de datos

--- db-CLIPsHOP.sql
+++ db-CLIPsHOP.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versión del servidor:         10.4.24-MariaDB - mariadb.org binary distribution
+-- Versión del servidor:         10.4.27-MariaDB - mariadb.org binary distribution
 -- SO del servidor:              Win64
 -- HeidiSQL Versión:             12.1.0.6537
 -- --------------------------------------------------------
@@ -16,7 +16,7 @@
 
 
 -- Volcando estructura de base de datos para clipshop
-CREATE DATABASE IF NOT EXISTS `clipshop` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE IF NOT EXISTS `clipshop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `clipshop`;
 
 -- Volcando estructura para tabla clipshop.banners
@@ -24,19 +24,19 @@ CREATE TABLE IF NOT EXISTS `banners` (
   `idBanners` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `banner` varchar(155) NOT NULL DEFAULT '0',
   PRIMARY KEY (`idBanners`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Volcando datos para la tabla clipshop.banners: ~2 rows (aproximadamente)
 INSERT INTO `banners` (`idBanners`, `banner`) VALUES
 	(9, 'Banner1669645146943.png'),
-	(10, 'Banner1670514381769.png');
+	(10, 'Banner1671467303544.png');
 
 -- Volcando estructura para tabla clipshop.categorias
 CREATE TABLE IF NOT EXISTS `categorias` (
   `idCategorias` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nombreCategoria` varchar(155) NOT NULL,
   PRIMARY KEY (`idCategorias`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Volcando datos para la tabla clipshop.categorias: ~4 rows (aproximadamente)
 INSERT INTO `categorias` (`idCategorias`, `nombreCategoria`) VALUES
@@ -52,9 +52,9 @@ CREATE TABLE IF NOT EXISTS `codigopostal` (
   `comuna` int(11) NOT NULL,
   `cp` int(10) unsigned NOT NULL,
   PRIMARY KEY (`idCodPost`)
-) ENGINE=InnoDB AUTO_INCREMENT=627 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=627 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla clipshop.codigopostal: ~626 rows (aproximadamente)
+-- Volcando datos para la tabla clipshop.codigopostal: ~34 rows (aproximadamente)
 INSERT INTO `codigopostal` (`idCodPost`, `barrio`, `comuna`, `cp`) VALUES
 	(1, 'Recoleta', 2, 1000),
 	(2, 'Recoleta', 2, 1001),
@@ -686,28 +686,60 @@ INSERT INTO `codigopostal` (`idCodPost`, `barrio`, `comuna`, `cp`) VALUES
 -- Volcando estructura para tabla clipshop.productos
 CREATE TABLE IF NOT EXISTS `productos` (
   `idProductos` int(20) unsigned NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(155) NOT NULL DEFAULT '0',
+  `nombre` varchar(255) NOT NULL DEFAULT '0',
   `FKidCategoria` int(10) unsigned NOT NULL DEFAULT 0,
+  `filtro` varchar(155) NOT NULL,
+  `subfiltro` varchar(155) DEFAULT NULL,
   `precio` decimal(20,2) NOT NULL DEFAULT 0.00,
-  `descripcion` text DEFAULT NULL,
+  `oferta` tinyint(1) DEFAULT 0,
   `imagen` varchar(155) NOT NULL,
-  `oferta` tinyint(2) DEFAULT 0,
-  `color` varchar(50) DEFAULT NULL,
-  `peso` decimal(20,2) DEFAULT NULL,
-  `medida` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`idProductos`),
   KEY `FKidCategorias` (`FKidCategoria`),
   CONSTRAINT `FKidCategorias` FOREIGN KEY (`FKidCategoria`) REFERENCES `categorias` (`idCategorias`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=528 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla clipshop.productos: ~7 rows (aproximadamente)
-INSERT INTO `productos` (`idProductos`, `nombre`, `FKidCategoria`, `precio`, `descripcion`, `imagen`, `oferta`, `color`, `peso`, `medida`) VALUES
-	(17, 'Cuadrado', 1, 50.00, 'rollo de papel', 'Producto1669646038787.jpg', 0, 'Rosa', 13.00, ''),
-	(19, 'Prueba de slide toggle', 3, 356.00, '<srfhbdzxfbzdthdgc', 'Producto1669646002557.jpg', 1, 'Transparente', 13.00, ''),
-	(20, 'Film', 3, 185.00, 'blab lab wed', 'Producto1669646020731.jpg', 1, 'Azul', 12.00, '25'),
-	(21, 'Cuadrado Rosa', 1, 12458.00, NULL, 'Producto1667744118309.jpg', 1, '', 25.00, ''),
-	(23, 'Cuadrado Rosa', 4, 12458.00, NULL, 'Producto1667745707853.jpg', 1, '', 10.00, ''),
-	(24, 'Cuadrado  Azul', 2, 12.00, NULL, 'Producto1667745971340.jpg', 1, 'Azul', 2.50, '20cm x 30cm');
+-- Volcando datos para la tabla clipshop.productos: ~40 rows (aproximadamente)
+INSERT INTO `productos` (`idProductos`, `nombre`, `FKidCategoria`, `filtro`, `subfiltro`, `precio`, `oferta`, `imagen`) VALUES
+	(458, 'Film Stretch Transparente- Rollo de 50 cm/2,5 kg', 1, 'film', 'stretch', 0.00, 1, 'Producto1669646038787.jpg'),
+	(459, 'Film Stretch Negro- Rollo de 50 cm/2,5 kg', 1, 'film', 'stretch', 0.00, 0, 'Producto1669646038787.jpg'),
+	(460, 'Film Stretch Verde- Rollo de 50 cm/2,5 kg', 1, 'film', 'stretch', 0.00, 0, 'Producto1669646038787.jpg'),
+	(461, 'Film Stretch Azul- Rollo de 50 cm/2,5 kg', 1, 'film', 'stretch', 0.00, 1, 'Producto1669646038787.jpg'),
+	(462, 'Film Stretch Rojo- Rollo de 50 cm/2,5 kg', 1, 'film', 'stretch', 0.00, 1, 'Producto1669646038787.jpg'),
+	(463, 'Film Stretch Transparente-C/Mango- Rollo de 50cm/2,6kg', 1, 'film', 'con mango', 0.00, 1, 'Producto1669646038787.jpg'),
+	(464, 'Film Stretch Negro-C/Mango- Rollo de 50cm/2,6kg', 1, 'film', 'con mango', 0.00, 1, 'Producto1669646038787.jpg'),
+	(465, 'Film Stretch Transparente- Rollo de 50 cm/4 kg', 1, 'film', 'stretch', 0.00, 1, 'Producto1669646038787.jpg'),
+	(466, 'Film Stretch Negro- Rollo de 50 cm/4 kg', 1, 'film', 'stretch', 0.00, 0, 'Producto1669646038787.jpg'),
+	(467, 'Film Stretch Verde- Rollo de 50 cm/4 kg', 1, 'film', 'stretch', 0.00, 0, 'Producto1669646038787.jpg'),
+	(468, 'Film Stretch Azul- Rollo de 50 cm/4 kg', 1, 'film', 'stretch', 0.00, 0, 'Producto1669646038787.jpg'),
+	(469, 'Film Stretch Rojo- Rollo de 50 cm/4 kg', 1, 'film', 'stretch', 0.00, 1, 'Producto1669646038787.jpg'),
+	(470, 'Film Stretch Transparente- Rollo de 10cm', 1, 'film', 'stretch', 0.00, 0, 'Producto1669646038787.jpg'),
+	(471, 'Film Stretch Negro- Rollo de 10cm', 1, 'film', 'stretch', 0.00, 1, 'Producto1669646038787.jpg'),
+	(472, 'Film Stretch Verde- Rollo de 10cm', 1, 'film', 'stretch', 0.00, 0, 'Producto1669646038787.jpg'),
+	(473, 'Film Stretch Azul- Rollo de 10cm', 1, 'film', 'stretch', 0.00, 1, 'Producto1669646038787.jpg'),
+	(474, 'Film Stretch Rojo- Rollo de 10cm', 1, 'film', 'stretch', 0.00, 0, 'Producto1669646038787.jpg'),
+	(475, 'Film Alimenticio 38x1000', 1, 'film', 'alimenticio', 0.00, 1, 'Producto1669646038787.jpg'),
+	(500, 'Expendedora de Números para Turnos- Roja', 2, 'expendedora', 'sin barral', 0.00, 0, 'Producto1669646038787.jpg'),
+	(501, 'Expendedora de Números para Turnos- Negra', 2, 'expendedora', 'sin barral', 0.00, 0, 'Producto1669646038787.jpg'),
+	(502, 'Expendedora de Números para Turnos- Blanca', 2, 'expendedora', 'sin barral', 0.00, 0, 'Producto1669646038787.jpg'),
+	(503, 'Expendedora de Números para Turnos- Gris', 2, 'expendedora', 'sin barral', 0.00, 1, 'Producto1669646038787.jpg'),
+	(504, 'Expendedora de Números para Turnos- Violeta', 2, 'expendedora', 'sin barral', 0.00, 0, 'Producto1669646038787.jpg'),
+	(505, 'Expendedora de Números para Turnos- Marrón', 2, 'expendedora', 'sin barral', 0.00, 1, 'Producto1669646038787.jpg'),
+	(506, 'Expendedora de Números para Turnos- Naranja', 2, 'expendedora', 'sin barral', 0.00, 0, 'Producto1669646038787.jpg'),
+	(507, 'Expendedora de Números para Turnos- Amarilla', 2, 'expendedora', 'sin barral', 0.00, 1, 'Producto1669646038787.jpg'),
+	(508, 'Expendedora De Números Para Turnos Con Pie Y Cartel', 2, 'expendedora', 'con barral', 0.00, 0, 'Producto1669646038787.jpg'),
+	(509, 'Expendedora De Números Para Turnos Con Pie Y Cartel + Pincha Papel', 2, 'expendedora', 'con barral', 0.00, 1, 'Producto1669646038787.jpg'),
+	(516, 'Etiquetadora Jolly-Jh8- 8 Dígitos', 4, 'etiquetadoras', 'etiquetadoras', 0.00, 1, 'Producto1669646038787.jpg'),
+	(517, 'Etiquetadora OpenComercial', 4, 'etiquetadoras', 'etiquetadoras', 0.00, 0, 'Producto1669646038787.jpg'),
+	(518, 'Entintador para Etiquetadora Jolly', 4, 'etiquetadoras', 'etintadores', 0.00, 0, 'Producto1669646038787.jpg'),
+	(519, 'Entintador OpenComercial', 4, 'etiquetadoras', 'etintadores', 0.00, 0, 'Producto1669646038787.jpg'),
+	(520, 'Rollo de Números para Turnos x 1000 Rojo', 2, 'rollos de numeros', 'x 1000', 0.00, 1, 'Producto1669646038787.jpg'),
+	(521, 'Rollo de Números para Turnos x 1000 Verde', 2, 'rollos de numeros', 'x 1000', 0.00, 0, 'Producto1669646038787.jpg'),
+	(522, 'Rollo de Números para Turnos x 1000 Azul', 2, 'rollos de numeros', 'x 1000', 0.00, 1, 'Producto1669646038787.jpg'),
+	(523, 'Rollo de Números para Turnos x 1000 Amarillo', 2, 'rollos de numeros', 'x 1000', 0.00, 0, 'Producto1669646038787.jpg'),
+	(524, 'Rollo de Números para Turnos x 2000 Rojo', 2, 'rollos de numeros', 'x 2000', 0.00, 1, 'Producto1669646038787.jpg'),
+	(525, 'Rollo de Números para Turnos x 2000 Verde', 2, 'rollos de numeros', 'x 2000', 0.00, 0, 'Producto1669646038787.jpg'),
+	(526, 'Rollo de Números para Turnos x 2000 Amarillo', 2, 'rollos de numeros', 'x 2000', 0.00, 1, 'Producto1669646038787.jpg'),
+	(527, 'Rollo de Números para Turnos x 2000 Azul', 2, 'rollos de numeros', 'x 2000', 0.00, 0, 'Producto1669646038787.jpg');
 
 -- Volcando estructura para tabla clipshop.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -716,33 +748,18 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `email` varchar(155) NOT NULL,
   `direccion` varchar(255) NOT NULL,
   `telefono` int(11) DEFAULT NULL,
-  `FKCodigoPostal` int(10) unsigned DEFAULT NULL,
   `password` varchar(155) NOT NULL,
   `rol` varchar(50) NOT NULL DEFAULT 'user',
-  PRIMARY KEY (`idUsuarios`),
-  KEY `FKCodigoPostal` (`FKCodigoPostal`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`idUsuarios`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla clipshop.usuarios: ~6 rows (aproximadamente)
-INSERT INTO `usuarios` (`idUsuarios`, `nombre`, `email`, `direccion`, `telefono`, `FKCodigoPostal`, `password`, `rol`) VALUES
-	(4, 'Ailen', 'cury.ailena@gmail.com', 'Avenida Estado de Israel 4487', 1168508686, NULL, '$2a$10$b7sFjxISyLrhJGkSMYHzHub/Z36c4JqVkeAD6fp7HSvE5OU30Onw.', 'user'),
-	(6, 'Ailen', 'ailu@ailu.com', 'Soy Administrador', 1168508686, NULL, '$2a$10$BRrCiQPiGWAIAxj7b8TYCewkG8eMKJWP3xTF.Ya4PsdgzimYJEgWu', 'admin'),
-	(9, 'Ailen Cury', 'ailen@gmail.com', 'Prueba dirección', 555555, NULL, '$2a$10$.p53ZobtisbEW7UY53PeQeDIuGqhdlswCK0Lvi81CIJL90C.PqxK.', 'user'),
-	(10, 'Noni', 'noni@gmail.com', 'Casa Noni', 1125689588, NULL, '$2a$10$idhhKteaOhDAUzrZhxsvTux/EbND5Y1ZQ4lRw5JWGaKjTCoYvlkCG', 'admin'),
-	(11, 'ailen', 'cury.ailena@gmail.com', 'Avenida Estado de Israel 4487', 1111, NULL, '$2a$10$3c1K5EwctubuoL5o1zGQjOGqox5fcl2xzlrDErLtfHoAMRIZB2lSW', 'user'),
-	(12, 'ahfxrykudlt.ify-u', 'cury.ailena@gmail.com', 'Avenida Estado de Israel 4487', 5555, NULL, '$2a$10$B80MsCkiLV.koQ31E78LFuarqAhBpHpbersEqFIV11IVRWr12RlF6', 'user');
-
--- Volcando estructura para tabla clipshop.ventas
-CREATE TABLE IF NOT EXISTS `ventas` (
-  `idVentas` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idProductos` int(10) NOT NULL,
-  `idUsuarios` int(10) NOT NULL,
-  `cantidad` int(10) NOT NULL,
-  `precio` int(10) NOT NULL,
-  PRIMARY KEY (`idVentas`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Volcando datos para la tabla clipshop.ventas: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla clipshop.usuarios: ~0 rows (aproximadamente)
+INSERT INTO `usuarios` (`idUsuarios`, `nombre`, `email`, `direccion`, `telefono`, `password`, `rol`) VALUES
+	(6, 'Ailen', 'ailu@ailu.com', 'Soy Administrador', 1168508686, '$2a$10$BRrCiQPiGWAIAxj7b8TYCewkG8eMKJWP3xTF.Ya4PsdgzimYJEgWu', 'admin'),
+	(9, 'Ailen Cury', 'ailen@gmail.com', 'Prueba dirección', 555555, '$2a$10$.p53ZobtisbEW7UY53PeQeDIuGqhdlswCK0Lvi81CIJL90C.PqxK.', 'user'),
+	(10, 'Noni', 'noni@gmail.com', 'Casa Noni', 1125689588, '$2a$10$idhhKteaOhDAUzrZhxsvTux/EbND5Y1ZQ4lRw5JWGaKjTCoYvlkCG', 'admin'),
+	(17, 'Ailen', 'cury.ailena@gmail.com', 'bmun6ybtrvecwd', 0, '$2a$10$DG9oDmTg0jy56mISR3nQtuOpZ7IP07iYnpXApP/Tg5p9oQHonOIne', 'user'),
+	(19, 'a', 'ailenaldanacury@gmail.com', 'b', 0, '$2a$10$wGLW7LUkSAnZk9CVoZS98OtHncek5kaTWhbowB0ohhIC2Wnda5D12', 'user');
 
 -- Volcando estructura para tabla clipshop.vouchers
 CREATE TABLE IF NOT EXISTS `vouchers` (
@@ -751,12 +768,12 @@ CREATE TABLE IF NOT EXISTS `vouchers` (
   `valor` int(20) NOT NULL,
   `fecha` date NOT NULL,
   PRIMARY KEY (`idVouchers`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla clipshop.vouchers: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla clipshop.vouchers: ~0 rows (aproximadamente)
 INSERT INTO `vouchers` (`idVouchers`, `voucher`, `valor`, `fecha`) VALUES
-	(5, 'estamosCreciendo2023', 15, '2022-11-28'),
-	(6, '', 150, '2022-12-08');
+	(1, 'navidadClipShop2022', 25, '2022-11-28'),
+	(5, 'estamosCreciendo2023', 15, '2022-11-28');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
